1ae0f4dd9f29499ed476b180766a0ed2740ba008
Braden Niksich <bniksich28@gmail.com>
2021-01-18 19:01:26 -0600

    Allow origin

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cryptoRouter = require('./routes/cryptoRoutes');
 const app = express();
+const cors = require('cors')
 const apiPort = 8000;
 
+app.use(cors());
 app.use('/api', cryptoRouter);
 
 app.listen(process.env.PORT || apiPort, () => console.log(`Server running on port ${8000}`))
